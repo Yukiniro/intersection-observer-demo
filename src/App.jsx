c6f61ae26dcece1f5effcdf30298c877751ffd34
Zhanghao <yukiniro@hotmail.com>
2022-12-07 20:01:34 +0800

    feat： 增加 IntersectionObserver 的使用

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
 import "./App.css";
+import { useRef, useEffect } from "react";
 
 function App() {
+  const boxRef = useRef(null);
+  const subcontentRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        console.log(entries);
+      },
+      {
+        root: boxRef.current,
+        threshold: 1,
+      }
+    );
+
+    const target = subcontentRef.current;
+    observer.observe(target);
+    return () => {
+      observer.unobserve(target);
+    };
+  }, []);
+
   return (
     <div className="App">
       <h1>Intersection Observer Demo</h1>
-      <div></div>
+      <div className="box" ref={boxRef}>
+        <div className="content">
+          <div className="subcontent" ref={subcontentRef}></div>
+        </div>
+      </div>
     </div>
   );
 }
