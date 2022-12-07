c8568bf4a41be16570130b745b588a4c0b1245d7
Zhanghao <yukiniro@hotmail.com>
2022-12-07 16:33:01 +0800

    feat: init commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Intersection Observer Demo</h1>
+      <div></div>
+    </div>
+  );
+}
+
+export default App;
